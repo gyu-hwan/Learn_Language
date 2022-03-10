e7e87a425292c87fa9b30d9be888c849388e4029
gyu_hwan <jeon@gyuhwan.com>
2022-03-10 13:29:40 +0900

    package

---
+++ R/chapter01/packages.R
@@ -0,0 +1,8 @@
+# dim(available.packages()) #패키지 개수 출력
+# available.packages() #패키지 목록 출력
+# sessionInfo() #Rsession 정보
+# install.packages("") #패키지 설치
+# library(stringr) #stringr패키지 로드 없을시 에러 발생
+# require(stringr) #stringr패키지 로드 없을시 경고 발생
+# search() #현재 로드된 패키지 목록 출력
+# remove.packages("") #패키지 제거
