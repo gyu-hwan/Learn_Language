fcb9a618704475de37ee67eac8b120298f666b70
Dongmul <jeon@gyuhwan.com>
2022-08-16 11:58:39 +0900

    c# 다시공부

---
+++ c#/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {/**
+                Console.WriteLine("몇번 회전할까요?");
+                var run = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("회전을 시작합니다.");
+                for (int i = 0; i < run; i++)
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(1, 0);
+                    Console.WriteLine("★");
+                    Console.SetCursorPosition(3, 0);
+                    Thread.Sleep(10);
+                    Console.Clear();
+                    Console.SetCursorPosition(2, 0);
+                    Console.WriteLine("★");
+                    Console.SetCursorPosition(3, 0);
+                    Thread.Sleep(10);
+                    Console.Clear();
+                    Console.SetCursorPosition(2, 1);
+                    Console.WriteLine("★");
+                    Console.SetCursorPosition(3, 0);
+                    Thread.Sleep(10);
+                    Console.Clear();
+                    Console.SetCursorPosition(1, 1);
+                    Console.WriteLine("★");
+                    Console.SetCursorPosition(3, 0);
+                    Thread.Sleep(10);
+                }
+                Console.Clear();
+                Console.WriteLine("회전을 완료하였습니다!");\
+                **/
+            } //머리 안돌아가는 코드
+            {
+                //int x = 1;
+                //while (x < 50)
+                //{
+                //    Console.Clear();
+                //    Console.SetCursorPosition(x, 5);
+
+                //    if (x % 3 == 0)
+                //        Console.WriteLine("__@");
+                //    else if (x % 3 == 1)
+                //        Console.WriteLine("_^@");
+                //    else
+                //        Console.WriteLine("^_@");
+
+                //    Thread.Sleep(100);
+                //    x++;
+                //}
+            } //p187 달팽이 움직이는 쉬운 코드 + 한줄주석 "Ctrl+k+c"  주석해제 "Ctrl+k+u" 단축키 기억남
+            {
+                //List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+                //list.Add(1);
+                //list.Add(2);
+                //list.Add(3);
+                //list.Remove(1);
+                //foreach (var item in list)
+                //{
+                //    Console.WriteLine(item);
+                //}
+            } //리스트 선언 코드
+            {
+                /*ProductHeader_ product = new ProductHeader_();*/
+            } //p222 물음표 해결 단축키 "ctrl+." // 괄호 주석 단축키  "Ctrl+Shift+/"
+
+        }
+    }
+}
